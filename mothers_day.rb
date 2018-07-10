f09d5a737fa55ac09917cb69d357966d7b521087
Andrew Wirt <awjwirt@gmail.com>
2018-07-10 20:39:30 +0000

    automatically backed up by learn

--- mothers_day.rb
+++ mothers_day.rb
@@ -1,7 +1,17 @@
 ## Define your method, mothers_day, below. Go through the README and update your method as needed!
 
-def mothers_day
-  puts "Happy Mother's Day, Mom!"
-end 
+# def mothers_day
+#   puts "Happy Mother's Day, Mom!"
+# end 
 
-mothers_day
+# mothers_day
+
+def say_hello (your_name = "bobecca", my_name = "rebecca")
+  puts "Hello #{your_name}, my name is #{my_name}"
+end
+# say_hello ("charlie")
+
+say_hello("brad","brody")
+say_hello(,"Rachana")
+say_hello()
+puts "how is life in canada <3? "
